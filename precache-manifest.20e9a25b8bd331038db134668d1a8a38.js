0ca8e9c833ca51c53989d21f2e2c7828e289b99a
Anila Qerimi <Gjogi jem>
2019-07-23 09:57:13 +0200

    Updates

---
+++ precache-manifest.20e9a25b8bd331038db134668d1a8a38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb01efbee59ccb731602ea2552fc154",
+    "url": "/DritonQ/WebApp/index.html"
+  },
+  {
+    "revision": "0f9302fa584c932502c3",
+    "url": "/DritonQ/WebApp/static/css/main.c305e3ff.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/DritonQ/WebApp/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "0f9302fa584c932502c3",
+    "url": "/DritonQ/WebApp/static/js/main.666dc5c8.chunk.js"
+  },
+  {
+    "revision": "90ac55cabe6d370362d5",
+    "url": "/DritonQ/WebApp/static/js/runtime~main.e55389ae.js"
+  }
+]);
